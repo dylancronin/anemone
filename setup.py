4063358bcd34d69fab7844db3cdb0be377184971
dylancronin <cronin.dylan.r@gmail.com>
2026-08-10 16:29:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ setup(
     description="A standardized command-line pipeline integrating WGCNA and PLS-VIP regression",
     author="Dylan Cronin",
     url="https://github.com/dylancronin/anemone",
+    py_modules=[],
+    packages=[],
     scripts=["anemone"],
     python_requires=">=3.8",
     install_requires=[
